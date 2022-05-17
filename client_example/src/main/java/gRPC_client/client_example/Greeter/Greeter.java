5344e8b01ed102fe6e8a5de9e8ad8cd1c3bae8be
ohjinhokor <ohjinho7@gmail.com>
2022-05-17 18:59:24 +0900

    서버, 클라이언트 생성

---
+++ client_example/src/main/java/gRPC_client/client_example/Greeter/Greeter.java
@@ -0,0 +1,4 @@
+package gRPC_client.client_example.Greeter;
+
+public class Greeter {
+}
---
+++ server_example/src/test/java/gRPC_server/server_example/ServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package gRPC_server.server_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
